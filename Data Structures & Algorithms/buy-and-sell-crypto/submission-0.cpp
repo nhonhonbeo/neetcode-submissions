cc292af5b97941eb73ef3d599c7a28689f430450
nhonhonbeo <ptanhan2309@gmail.com>
2026-05-04 23:00:46 +0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int min = prices[0];
+        int profit = 0;
+        int max_profit = 0;
+        for(int i = 0; i<n; i++){
+            if(prices[i]<min) min = prices[i];
+            else profit = prices[i] - min;
+            if(profit > max_profit) max_profit = profit;
+        }
+        return max_profit;
+    }
+};
