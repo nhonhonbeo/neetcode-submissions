7883e67890aee3ac38bf96672a3663a2463c74da
nhonhonbeo <ptanhan2309@gmail.com>
2026-05-05 14:22:40 +0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int left = 0; 
+        int right = 0;
+        int max_profit = 0;
+        while(right < prices.size()){
+            if(prices[left] > prices[right]) left = right;
+            else{
+                int profit = prices[right] - prices[left];
+                if(profit > max_profit) max_profit = profit;
+            }
+            right++;
+        }
+        return max_profit;
+    }
+};
